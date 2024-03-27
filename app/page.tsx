9e808a4f4e714859aad9b3f38a28b6ba5e2cb2c6
Omkar-S-Kirane <omkarofficial.ok@gmail.com>
2024-03-28 01:03:58 +0530

    demo changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ type User = {
 }
 
 
-export default function Counter() {
+export default function HomePage() {
   const [userData, setUserData] = useState<User[]>([]);
   const colorArray: string[] = ['#8e24aa', '#e53935', '#d81b60'];
 
@@ -67,42 +67,42 @@ export default function Counter() {
     <div className='row' style={{ padding: '12px'}}>
       <Grid>
         {
-          userData.length > 0 && 
-            userData.map((user, userIndex) => {
-              let userNameInitial = user.name
-                .split(" ")
-                .map((word, index, array) =>
-                  index === 0 || index === array.length - 1 ? word[0] : null
-                )
-                .join("");
-              return(
-                <Grid.Col key={userIndex} span={{ base: 12, xl:3,lg:3, md:6, xs: 6 }}>
-                  <Card withBorder shadow="sm" radius="md" padding='md' style={{ marginBottom: '16px',minHeight:'60vh',maxHeight:'60vh' }}>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',paddingTop:'20px' }}>
-                      <div title={ user.name} className='mt-2' style={{ width: '120px', height: '120px', borderRadius: '50%', backgroundColor: colorArray[userIndex % colorArray.length], display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <span  style={{ color: 'white', fontSize: '60px', cursor:'pointer'}}>
-                          {userNameInitial}
-                        </span>
-                      </div>
-                      <text style={{ textAlign: 'center', marginTop: '20px',marginBottom:'10px',fontSize:'20px' }}>{user.name || ''}{" "}{user.followStatus == true && <CiStar />}</text>
-                      <div style={{textAlign:'left',width:'95%'}}>
-                        <div onClick={()=>handleEmailClick(user.email)} style={{marginBottom:'7px',color:'rgb(145 145 145)',cursor:'pointer'}}>@ {user.email || ''}</div>
-                        <span style={{marginBottom:'7px',color:'rgb(145 145 145)',cursor:'pointer'}}> <MdOutlineWifiCalling3 style={{ marginRight: '8px' }} /> {user.phone || ''}</span>
-                        <div> <ImSphere style={{ marginRight: '8px' }} /><a href={user.website} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline',color:'rgb(145 145 145)',cursor:'pointer' }}>{user.website}</a></div>
-                        <div style={{ textAlign: 'center', marginTop: '20px', marginBottom: '20px',display:'flex' }}>
-                          <Button onClick={()=>storeFollowedUser(userIndex)} style={{background:user.followStatus == false ? '#228be6' : 'white',color:user.followStatus == false ? 'white' : "",marginRight:'5px',cursor:'pointer'}} fullWidth gradient={{from: 'blue', to: 'blue', deg: 90} } leftSection={<TbUserPlus size={14} />} variant="default">
-                            {user.followStatus == false ? 'Follow' : 'Unfollow'}
-                          </Button>
-                          <Button onClick={()=>removeUserAtIndex(userIndex)} style={{background:'white',cursor:'pointer'}} fullWidth gradient={{from: 'blue', to: 'blue', deg: 90} } leftSection={<CiTrash size={14} />} variant="default">
-                            Delete
-                          </Button>
-                        </div>
-                      </div>
+        userData.length > 0 && userData.map((user, userIndex) => {
+          console.log(user)
+          let userNameInitial = user.name
+            .split(" ")
+            .map((word, index, array) =>
+              index === 0 || index === array.length - 1 ? word[0] : null
+            )
+            .join("");
+          return(
+            <Grid.Col key={userIndex} span={{ base: 12, xl:3,lg:3, md:6, xs: 6 }}>
+              <Card withBorder shadow="sm" radius="md" padding='md' style={{ marginBottom: '16px',minHeight:'60vh',maxHeight:'60vh' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',paddingTop:'20px' }}>
+                  <div title={ user.name} className='mt-2' style={{ width: '120px', height: '120px', borderRadius: '50%', backgroundColor: colorArray[userIndex % colorArray.length], display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <span  style={{ color: 'white', fontSize: '60px', cursor:'pointer'}}>
+                      {userNameInitial}
+                    </span>
+                  </div>
+                  <text style={{ textAlign: 'center', marginTop: '20px',marginBottom:'10px',fontSize:'20px' }}>{user.name || ''}{" "}{user.followStatus == true && <CiStar />}</text>
+                  <div style={{textAlign:'left',width:'95%'}}>
+                    <div onClick={()=>handleEmailClick(user.email)} style={{marginBottom:'7px',color:'rgb(145 145 145)',cursor:'pointer'}}>@ {user.email || ''}</div>
+                    <span style={{marginBottom:'7px',color:'rgb(145 145 145)',cursor:'pointer'}}> <MdOutlineWifiCalling3 style={{ marginRight: '8px' }} /> {user.phone || ''}</span>
+                    <div> <ImSphere style={{ marginRight: '8px' }} /><a href={user.website} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline',color:'rgb(145 145 145)',cursor:'pointer' }}>{user.website}</a></div>
+                    <div style={{ textAlign: 'center', marginTop: '20px', marginBottom: '20px',display:'flex' }}>
+                      <Button onClick={()=>storeFollowedUser(userIndex)} style={{background:user.followStatus == false ? '#228be6' : 'white',color:user.followStatus == false ? 'white' : "",marginRight:'5px',cursor:'pointer'}} fullWidth gradient={{from: 'blue', to: 'blue', deg: 90} } leftSection={<TbUserPlus size={14} />} variant="default">
+                        {user.followStatus == false ? 'Follow' : 'Unfollow'}
+                      </Button>
+                      <Button onClick={()=>removeUserAtIndex(userIndex)} style={{background:'white',cursor:'pointer'}} fullWidth gradient={{from: 'blue', to: 'blue', deg: 90} } leftSection={<CiTrash size={14} />} variant="default">
+                        Delete
+                      </Button>
                     </div>
-                  </Card>
-                </Grid.Col>
-              )
-            })
+                  </div>
+                </div>
+              </Card>
+            </Grid.Col>
+          )
+        })
         }
       </Grid>
     </div>
